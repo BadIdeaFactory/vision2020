055f7c24459d94a53f1edf1339e077af3d58a1fe
Lou Huang <lou@louhuang.com>
2019-10-09 21:22:07 -0400

    static routing

--- next.config.js
+++ next.config.js
@@ -3,9 +3,39 @@ module.exports = {
     // Let Next.js know where to find the entry page
     // when it's exporting the static bundle for the use
     // in the production version of your app
-    return {
-      '/': { page: '/' }
+    const slugs = [
+      'mary-church-terrell',
+      'florynce-kennedy',
+      'emma-gonzalez',
+      'jeannette-rankin',
+      'shirley-chisholm',
+      'women-in-the-116th-us-congress',
+      'alva-belmont',
+      'indra-nooyi',
+      'arlan-hamilton',
+      'grace-hopper',
+      'reshma-saujani',
+      'sabrina-gonzalez-pasterski',
+      'ida-b-wells',
+      'gloria-steinem',
+      'brittney-c-cooper',
+      'bernice-sandler',
+      'dawn-staley',
+      'mone-davis'
+    ]
+
+    const paths = {
+      '/': { page: '/' },
+      '/credits': { page: '/credits' },
+      '/pioneers': { page: '/pioneers' },
+      '/vote': { page: '/vote' }
     }
+
+    slugs.forEach(slug => {
+      paths[`/pioneers/${slug}`] = { page: '/pioneers/[id]', query: { id: slug } }
+    })
+
+    return paths
   },
   devIndicators: {
     autoPrerender: false
