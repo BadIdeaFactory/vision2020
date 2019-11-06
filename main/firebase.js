54ddac86798ceccd90101d9eee1b7f3aa5ed576a
Lou Huang <lou@louhuang.com>
2019-11-06 16:42:20 -0500

    fix(firebase): disable analytics for server build

--- main/firebase.js
+++ main/firebase.js
@@ -2,7 +2,8 @@
 import * as firebase from 'firebase/app'
 
 // If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import 'firebase/analytics'
+// Disable this module because it breaks on build
+// import 'firebase/analytics'
 
 // Add the Firebase products that you want to use
 import 'firebase/firestore'
@@ -25,7 +26,7 @@ function init (config) {
   // Initialize Firebase
   console.log('[Firebase] Initializing ...')
   firebase.initializeApp(config)
-  firebase.analytics()
+  // firebase.analytics()
   firebase.firestore().enablePersistence({ synchronizeTabs: true })
     .then(() => {
       console.log('[Firebase] Database persistence enabled!')
