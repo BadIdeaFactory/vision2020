ee9cc485c99b6819b4ce36822e789a03d82e69db
Lou Huang <lou@louhuang.com>
2020-01-03 07:48:06 -0500

    refactor: use firebase credentials from env vars

--- main/firebase.js
+++ main/firebase.js
@@ -10,14 +10,14 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const config = {
-  apiKey: 'AIzaSyC2pKf7gPdUB8M9QwKZxrKcwFoa37GCEKw',
-  authDomain: 'vision2020-8ee3c.firebaseapp.com',
-  databaseURL: 'https://vision2020-8ee3c.firebaseio.com',
-  projectId: 'vision2020-8ee3c',
-  storageBucket: 'vision2020-8ee3c.appspot.com',
-  messagingSenderId: '586031075770',
-  appId: '1:586031075770:web:36309cd12e85e5a33f0d83',
-  measurementId: 'G-P0S4CG3BS5'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 console.log('[Firebase] Importing module ...')
--- next.config.js
+++ next.config.js
@@ -95,7 +95,16 @@ const nextConfig = {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     KIOSK_MODE: parse(process.env.KIOSK_MODE),
     KIOSK_ID: parse(process.env.KIOSK_ID),
-    DEV_ADA_WIREFRAME: parse(process.env.DEV_ADA_WIREFRAME)
+    DEV_ADA_WIREFRAME: parse(process.env.DEV_ADA_WIREFRAME),
+    // Firebase
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
   },
   devIndicators: {
     autoPrerender: false
